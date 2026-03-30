1b37ff24035c61e19394021d6d655e4f6093002b
Eliene Maria <kiancaraja@hotmail.com>
2026-03-30 00:39:54 -0300

    feat: estrutura inicial do projeto e conexão com mongodb

---
+++ OneDrive/Documentos/Back end/SGEV-Sistema-Gestao-Estoque/src/configs/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("✅ Shark, o banco de dados SGEV conectou com sucesso!");
+    } catch (error) {
+        console.error("❌ Erro ao conectar ao banco:", error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
